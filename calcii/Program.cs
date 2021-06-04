f9227a93b6dfc9c880d0e958a6bdeee79a091c58
Simran Preet Kaur <simranpreetk917@gmail.com>
2021-06-04 16:14:37 +0530

    changes

---
+++ calcii/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calcii
+{
+    public class Calc
+    {
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+        public int sub(int a, int b)
+        {
+            return a - b;
+        }
+        public int mul(int a, int b)
+        {
+            return a * b;
+        }
+        public int div(int a, int b)
+        {
+            return a / b;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calc obj = new Calc();
+            obj.add(2, 3);
+            Console.WriteLine(obj.add(2, 3));
+            Console.ReadKey();
+        }
+    }
+}
